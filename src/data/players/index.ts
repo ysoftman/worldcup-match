6c79f69ad0b5ca0cb7c50aef63feadb7acd2ea60
ysoftman <ysoftman@gmail.com>
2026-04-14 13:01:08 +0900

    add player data for 26 new countries

--- src/data/players/index.ts
+++ src/data/players/index.ts
@@ -1,23 +1,31 @@
 import type { RawPlayer } from "../../utils/playerRating";
+import ad from "./ad.json";
 import ae from "./ae.json";
+import ag from "./ag.json";
 import al from "./al.json";
 import am from "./am.json";
 import ao from "./ao.json";
 import ar from "./ar.json";
 import at from "./at.json";
 import au from "./au.json";
+import aw from "./aw.json";
 import az from "./az.json";
 import ba from "./ba.json";
+import bb from "./bb.json";
+import bd from "./bd.json";
 import be from "./be.json";
 import bf from "./bf.json";
 import bg from "./bg.json";
 import bh from "./bh.json";
 import bi from "./bi.json";
 import bj from "./bj.json";
+import bm from "./bm.json";
 import bo from "./bo.json";
 import br from "./br.json";
+import bt from "./bt.json";
 import bw from "./bw.json";
 import by from "./by.json";
+import bz from "./bz.json";
 import ca from "./ca.json";
 import cd from "./cd.json";
 import cg from "./cg.json";
@@ -42,6 +50,7 @@ import eg from "./eg.json";
 import es from "./es.json";
 import et from "./et.json";
 import fi from "./fi.json";
+import fj from "./fj.json";
 import fo from "./fo.json";
 import fr from "./fr.json";
 import ga from "./ga.json";
@@ -49,8 +58,10 @@ import gb_eng from "./gb-eng.json";
 import gb_nir from "./gb-nir.json";
 import gb_sct from "./gb-sct.json";
 import gb_wls from "./gb-wls.json";
+import gd from "./gd.json";
 import ge from "./ge.json";
 import gh from "./gh.json";
+import gi from "./gi.json";
 import gm from "./gm.json";
 import gn from "./gn.json";
 import gr from "./gr.json";
@@ -73,10 +84,13 @@ import jo from "./jo.json";
 import jp from "./jp.json";
 import ke from "./ke.json";
 import kg from "./kg.json";
+import kh from "./kh.json";
 import kr from "./kr.json";
 import kw from "./kw.json";
 import kz from "./kz.json";
+import la from "./la.json";
 import lb from "./lb.json";
+import li from "./li.json";
 import lr from "./lr.json";
 import ls from "./ls.json";
 import lt from "./lt.json";
@@ -84,11 +98,16 @@ import lu from "./lu.json";
 import lv from "./lv.json";
 import ly from "./ly.json";
 import ma from "./ma.json";
+import md from "./md.json";
 import me from "./me.json";
 import mk from "./mk.json";
 import ml from "./ml.json";
 import mm from "./mm.json";
+import mn from "./mn.json";
 import mr from "./mr.json";
+import mt from "./mt.json";
+import mu from "./mu.json";
+import mv from "./mv.json";
 import mw from "./mw.json";
 import mx from "./mx.json";
 import my from "./my.json";
@@ -97,11 +116,13 @@ import ng from "./ng.json";
 import ni from "./ni.json";
 import nl from "./nl.json";
 import no_ from "./no.json";
+import np from "./np.json";
 import nz from "./nz.json";
 import om from "./om.json";
 import pa from "./pa.json";
 import pe from "./pe.json";
 import ph from "./ph.json";
+import pk from "./pk.json";
 import pl from "./pl.json";
 import ps from "./ps.json";
 import pt from "./pt.json";
@@ -117,7 +138,10 @@ import se from "./se.json";
 import sg from "./sg.json";
 import si from "./si.json";
 import sk from "./sk.json";
+import sm from "./sm.json";
 import sn from "./sn.json";
+import so from "./so.json";
+import sr from "./sr.json";
 import sv from "./sv.json";
 import sy from "./sy.json";
 import tg from "./tg.json";
@@ -127,6 +151,7 @@ import tm from "./tm.json";
 import tn from "./tn.json";
 import tr from "./tr.json";
 import tt from "./tt.json";
+import tw from "./tw.json";
 import tz from "./tz.json";
 import ua from "./ua.json";
 import ug from "./ug.json";
@@ -136,30 +161,39 @@ import uz from "./uz.json";
 import ve from "./ve.json";
 import vn from "./vn.json";
 import xk from "./xk.json";
+import ye from "./ye.json";
 import za from "./za.json";
 import zm from "./zm.json";
 import zw from "./zw.json";
 
 const playersMap: Record<string, RawPlayer[]> = {
+	AD: ad,
 	AE: ae,
+	AG: ag,
 	AL: al,
 	AM: am,
 	AO: ao,
 	AR: ar,
 	AT: at,
 	AU: au,
+	AW: aw,
 	AZ: az,
 	BA: ba,
+	BB: bb,
+	BD: bd,
 	BE: be,
 	BF: bf,
 	BG: bg,
 	BH: bh,
 	BI: bi,
 	BJ: bj,
+	BM: bm,
 	BO: bo,
 	BR: br,
+	BT: bt,
 	BW: bw,
 	BY: by,
+	BZ: bz,
 	CA: ca,
 	CD: cd,
 	CG: cg,
@@ -184,6 +218,7 @@ const playersMap: Record<string, RawPlayer[]> = {
 	ES: es,
 	ET: et,
 	FI: fi,
+	FJ: fj,
 	FO: fo,
 	FR: fr,
 	GA: ga,
@@ -191,8 +226,10 @@ const playersMap: Record<string, RawPlayer[]> = {
 	"GB-NIR": gb_nir,
 	"GB-SCT": gb_sct,
 	"GB-WLS": gb_wls,
+	GD: gd,
 	GE: ge,
 	GH: gh,
+	GI: gi,
 	GM: gm,
 	GN: gn,
 	GR: gr,
@@ -215,10 +252,13 @@ const playersMap: Record<string, RawPlayer[]> = {
 	JP: jp,
 	KE: ke,
 	KG: kg,
+	KH: kh,
 	KR: kr,
 	KW: kw,
 	KZ: kz,
+	LA: la,
 	LB: lb,
+	LI: li,
 	LR: lr,
 	LS: ls,
 	LT: lt,
@@ -226,11 +266,16 @@ const playersMap: Record<string, RawPlayer[]> = {
 	LV: lv,
 	LY: ly,
 	MA: ma,
+	MD: md,
 	ME: me,
 	MK: mk,
 	ML: ml,
 	MM: mm,
+	MN: mn,
 	MR: mr,
+	MT: mt,
+	MU: mu,
+	MV: mv,
 	MW: mw,
 	MX: mx,
 	MY: my,
@@ -239,11 +284,13 @@ const playersMap: Record<string, RawPlayer[]> = {
 	NI: ni,
 	NL: nl,
 	NO: no_,
+	NP: np,
 	NZ: nz,
 	OM: om,
 	PA: pa,
 	PE: pe,
 	PH: ph,
+	PK: pk,
 	PL: pl,
 	PS: ps,
 	PT: pt,
@@ -259,7 +306,10 @@ const playersMap: Record<string, RawPlayer[]> = {
 	SG: sg,
 	SI: si,
 	SK: sk,
+	SM: sm,
 	SN: sn,
+	SO: so,
+	SR: sr,
 	SV: sv,
 	SY: sy,
 	TG: tg,
@@ -269,6 +319,7 @@ const playersMap: Record<string, RawPlayer[]> = {
 	TN: tn,
 	TR: tr,
 	TT: tt,
+	TW: tw,
 	TZ: tz,
 	UA: ua,
 	UG: ug,
@@ -278,6 +329,7 @@ const playersMap: Record<string, RawPlayer[]> = {
 	VE: ve,
 	VN: vn,
 	XK: xk,
+	YE: ye,
 	ZA: za,
 	ZM: zm,
 	ZW: zw,
